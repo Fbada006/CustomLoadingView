51a930e63c7ae10e884be494c1217dac08eebd3e
Fbada006 <ferdinandkamata@yahoo.com>
2021-08-27 12:48:27 +0300

    add button to enable/disable loading animation

--- app/src/main/java/com/example/customloadingview/MainActivity.kt
+++ app/src/main/java/com/example/customloadingview/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.customloadingview
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.customloadingview.customloadingview.LoadingView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loadingView = findViewById<LoadingView>(R.id.loadingView)
+        val button = findViewById<Button>(R.id.startStopAnimation)
+        button.setOnClickListener {
+            if (button.text == "Load") {
+                loadingView.showLoading()
+            } else {
+                loadingView.hideLoading()
+            }
+            button.text = "Stop load"
+        }
     }
 }
--- app/src/main/java/com/example/customloadingview/customloadingview/LoadingView.kt
+++ app/src/main/java/com/example/customloadingview/customloadingview/LoadingView.kt
@@ -1,17 +1,21 @@
 package com.example.customloadingview.customloadingview
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.annotation.Nullable
 import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import com.example.customloadingview.R
 
 class LoadingView : View {
     private lateinit var paint: Paint
     private var circleRadius = 100F
+    private var valueAnimator: ValueAnimator? = null
 
     constructor(context: Context) : super(context) {
         init(context, null)
@@ -33,5 +37,25 @@ class LoadingView : View {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawCircle(width / 2F, height / 2F, circleRadius, paint)
+    }
+
+    fun showLoading() {
+        isVisible = true
+        valueAnimator = ValueAnimator.ofFloat(10F, circleRadius).apply {
+            duration = 1000
+            interpolator = AccelerateDecelerateInterpolator()
+            addUpdateListener { animation ->
+                circleRadius = animation.animatedValue as Float
+                animation.repeatCount = ValueAnimator.INFINITE
+                animation.repeatMode = ValueAnimator.REVERSE
+                invalidate()
+            }
+            start()
+        }
+    }
+
+    fun hideLoading() {
+        isVisible = false
+        valueAnimator?.end()
     }
 }
